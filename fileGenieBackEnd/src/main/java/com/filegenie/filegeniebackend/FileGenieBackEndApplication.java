35eabf2b852788e8a65bb7724ea7f84793d9eadf
mouadboufenzi <mouadboufenzi2001@gmail.com>
2024-10-06 21:34:05 +0200

    first push

---
+++ fileGenieBackEnd/src/main/java/com/filegenie/filegeniebackend/FileGenieBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.filegenie.filegeniebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileGenieBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileGenieBackEndApplication.class, args);
+	}
+
+}
---
+++ fileGenieBackEnd/src/test/java/com/filegenie/filegeniebackend/FileGenieBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filegenie.filegeniebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileGenieBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
