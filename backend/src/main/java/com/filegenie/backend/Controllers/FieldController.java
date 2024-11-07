ad3893142e95800fb4b2d94cc8ed10bb811deed8
Julien Constant <julienconstant190@gmail.com>
2024-11-07 17:59:19 +0100

    add Swagger UI support to see all endpoints

--- backend/src/main/java/com/filegenie/backend/Controllers/FieldController.java
+++ backend/src/main/java/com/filegenie/backend/Controllers/FieldController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/field")
+@RequestMapping(path="/api/field")
 public class FieldController {
     @Autowired
     private FieldRepository fieldRepository;
