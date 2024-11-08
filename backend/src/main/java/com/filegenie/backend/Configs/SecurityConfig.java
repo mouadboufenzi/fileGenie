149f260e684b42654eb0dced49af40890f280892
Julien Constant <julienconstant190@gmail.com>
2024-11-08 17:09:11 +0100

    allow CORS for localhost:5173 (frontend dev)

--- backend/src/main/java/com/filegenie/backend/Configs/SecurityConfig.java
+++ backend/src/main/java/com/filegenie/backend/Configs/SecurityConfig.java
@@ -5,10 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -16,6 +27,9 @@ public class SecurityConfig {
             // CSRF does not work well with Swagger UI
             // TODO: find a way to make it works without disabling it
             .csrf().disable()
+            // CORS
+            // TODO: activate on production
+            .cors().and()
             // Turn Spring auth login panel off
             // TODO: activate on production
             .authorizeHttpRequests(request -> request.anyRequest().permitAll());
