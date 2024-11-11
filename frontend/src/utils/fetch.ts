cea5636588dfe350fefe16126c2fcae1275b6abc
Julien Constant <julienconstant190@gmail.com>
2024-11-11 19:48:39 +0100

    feat: add profile page

--- frontend/src/utils/fetch.ts
+++ frontend/src/utils/fetch.ts
@@ -1,12 +1,16 @@
 import { HttpException } from '../types/httpException';
 import { showNotification } from './show-notification';
 
-export async function fetchAPI<T extends object>(url: `/api/${string}`, method: 'GET' | 'POST', body?: Record<string, unknown>) {
-  
+export async function fetchAPI<T extends object>(url: `/api/${string}`, method: 'GET' | 'POST' | 'PUT', body?: Record<string, unknown>) {
+  const authToken = JSON.parse(sessionStorage.getItem('filegenie-token') ?? '{ "token": "" }') as { token: string };
+
   // TODO: move url to .env
   return fetch(`http://localhost:8080${url}`, {
     method,
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${authToken.token}`,
+    },
     body: JSON.stringify(body)
   })
     .then((response) => response.json())
