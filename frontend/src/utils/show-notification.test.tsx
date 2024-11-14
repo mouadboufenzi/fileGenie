bcc802974971d3aafcd884f46af03f65c3814b03
Julien Constant <julienconstant190@gmail.com>
2024-11-14 17:05:33 +0100

    tests: add some tests

--- frontend/src/utils/show-notification.test.tsx
+++ frontend/src/utils/show-notification.test.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+ 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import { describe, it, vi, expect } from 'vitest';
