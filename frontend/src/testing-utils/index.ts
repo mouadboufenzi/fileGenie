783f15f522b724f0a3c4e00dd0c145b3622c0ce8
Julien Constant <julienconstant190@gmail.com>
2024-11-14 17:05:33 +0100

    tests: add some tests

--- frontend/src/testing-utils/index.ts
+++ frontend/src/testing-utils/index.ts
@@ -2,5 +2,5 @@ import userEvent from '@testing-library/user-event';
 
 export * from '@testing-library/react';
 
-export { render, route } from './render.test';
+export { render } from './render.test';
 export { userEvent };
--- frontend/src/utils/show-notification.tsx
+++ frontend/src/utils/show-notification.tsx
@@ -3,7 +3,7 @@ import { notifications } from '@mantine/notifications';
 import { IoIosCheckmark, IoIosClose } from 'react-icons/io';
 
 export function showNotification(message: string, success = true) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+   
   notifications.show({
     message: <Text c="white">{message}</Text>,
     icon: success ? <IoIosCheckmark size={80} /> : <IoIosClose size={80} />,
