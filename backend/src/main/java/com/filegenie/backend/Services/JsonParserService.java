c917307e984cd0df4a06f086d9feb07c9ed39951
Morgan-sama <morganeangoc@gmail.com>
2024-11-19 09:28:14 +0100

    Ajustements dans JsonParserService.java (Ajout d'une vérification des fichiers et gestion des erreurs)

--- backend/src/main/java/com/filegenie/backend/Services/JsonParserService.java
+++ backend/src/main/java/com/filegenie/backend/Services/JsonParserService.java
@@ -1,7 +1,9 @@
 package com.filegenie.backend.Services;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.filegenie.backend.DTO.HttpException;
 import com.filegenie.backend.Entities.Field;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -11,10 +13,19 @@ import java.util.List;
 @Service
 public class JsonParserService {
 
-    public List<Field> parseJsonToFields(File jsonFile) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(jsonFile);
-        return processNode(null, rootNode);
+    public List<Field> parseJsonToFields(File jsonFile) throws HttpException {
+
+        if (jsonFile == null || !jsonFile.exists() || !jsonFile.isFile()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "Le fichier JSON fourni est invalide ou inexistant.");
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(jsonFile);
+            return processNode(null, rootNode);
+        } catch (Exception e) {
+            throw new HttpException(HttpStatus.INTERNAL_SERVER_ERROR, "Impossible de parser le fichier JSON: " + e.getMessage());
+        }
     }
 
     private List<Field> processNode(Field parent, JsonNode node) {
