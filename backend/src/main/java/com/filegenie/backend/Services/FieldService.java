3c4a8e015d59d1703bb42ec8f9f673e47ebdd04e
mouadboufenzi <mouadboufenzi2001@gmail.com>
2025-01-05 15:49:06 +0100

    Add /tree GET and PUT endpoints for field hierarchy management

--- backend/src/main/java/com/filegenie/backend/Services/FieldService.java
+++ backend/src/main/java/com/filegenie/backend/Services/FieldService.java
@@ -4,10 +4,13 @@ import com.filegenie.backend.Entities.Field;
 import com.filegenie.backend.Entities.FieldValue;
 import com.filegenie.backend.Repositories.FieldRepository;
 import com.filegenie.backend.Repositories.FieldValueRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class FieldService {
@@ -36,4 +39,55 @@ public class FieldService {
     public List<FieldValue> getFieldValues(Long fieldId) {
         return fieldValueRepository.findByField_FieldId(fieldId);
     }
+
+    @Transactional
+    public void updateFieldsTree(Map<String, Object> updatedTree) {
+        List<Map<String, Object>> fields = (List<Map<String, Object>>) updatedTree.get("fields");
+        if (fields != null) {
+            for (Map<String, Object> fieldData : fields) {
+                processFieldFromMap(null, fieldData);
+            }
+        }
+    }
+
+    @Transactional
+    public void processFieldFromMap(Field parent, Map<String, Object> fieldData) {
+        Field field = new Field();
+        field.setName((String) fieldData.get("name"));
+        field.setType((String) fieldData.get("type"));
+        field.setParentField(parent);
+
+
+        Optional<Field> existingFieldOptional = fieldRepository.findByName(field.getName());
+        if (existingFieldOptional.isPresent()) {
+            field = existingFieldOptional.get();
+            field.setType((String) fieldData.get("type"));
+        } else {
+            field = fieldRepository.save(field);
+        }
+
+
+        List<String> values = (List<String>) fieldData.get("values");
+        if (values != null) {
+
+            fieldValueRepository.deleteByField(field);
+
+
+            for (String value : values) {
+                FieldValue fieldValue = new FieldValue();
+                fieldValue.setValue(value);
+                fieldValue.setField(field);
+                fieldValueRepository.save(fieldValue);
+            }
+        }
+
+
+        List<Map<String, Object>> subFields = (List<Map<String, Object>>) fieldData.get("children");
+        if (subFields != null) {
+            for (Map<String, Object> subFieldData : subFields) {
+                processFieldFromMap(field, subFieldData);
+            }
+        }
+    }
+
 }
