868c2c5056e441619c424c33d5aea4f7c8ada914
Julien Constant <julienconstant190@gmail.com>
2025-01-08 19:16:34 +0100

    feat: link configuration saving with backend

--- frontend/src/routes/createfile.tsx
+++ frontend/src/routes/createfile.tsx
@@ -1,8 +1,8 @@
-import { createFileRoute, Navigate } from '@tanstack/react-router';
+import { createFileRoute, Navigate, useNavigate } from '@tanstack/react-router';
 import { useAuth } from '../auth-provider';
 import { useForm } from '@mantine/form';
 import { Stack, TextInput, Text, Button } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useTransition } from 'react';
 import { Field, FieldType } from '../types/field';
 import { fetchAPI } from '../utils/fetch';
 import { FieldContainer, FieldWithValues } from '../components/fieldContainer';
@@ -23,11 +23,14 @@ export const Route = createFileRoute('/createfile')({
 });
 
 function CreateFile() {
-  const { isAuthenticated } = useAuth();
-
-  const [allFields, setAllFields] = useState<Field[]>([]);
   /* eslint-disable-next-line */
   const { type } = Route.useSearch() as SearchParams;
+  const { isAuthenticated } = useAuth();
+  const [isLoading, startTransition] = useTransition();
+  const navigate = useNavigate();
+
+  const [config, setConfig] = useState<FieldWithValues[]>([]);
+  const [allFields, setAllFields] = useState<Field[]>([]);
 
   useEffect(() => {
     void fetchAPI<Field[]>('/api/field/all', 'GET').then((data) => {
@@ -45,14 +48,21 @@ function CreateFile() {
   });
 
   const handleSubmit = () => {
-    form.setFieldValue('configType', type.toUpperCase());
-    
-    // TODO: integrate with the backend
-    console.log(form.getValues(), config);
-  };
+    startTransition(() => {
+      form.setFieldValue('configType', type.toUpperCase());
 
-  // TODO: integrate with the backend
-  const [config, setConfig] = useState<FieldWithValues[]>([]);
+      void fetchAPI('/api/config/save', 'POST', {
+        configurationName: form.getValues().configurationName,
+        configurationType: form.getValues().configType,
+        configuration: JSON.stringify(config),
+      })
+        .then((data) => {
+          if ('error' in data) console.error(data);
+          else void navigate({ to: '/profile' });
+        });
+    });
+
+  };
 
   if (!isAuthenticated) return <Navigate to="/profile" />;
 
@@ -84,7 +94,7 @@ function CreateFile() {
           onConfigChange={(config) => setConfig(config)}
         />
 
-        <Button fullWidth color="violet" variant="light" type="submit">
+        <Button loading={isLoading} fullWidth color="violet" variant="light" type="submit">
           Sauvegarder
         </Button>
       </Stack>
